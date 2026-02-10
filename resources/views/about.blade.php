d17442dd96b1f605ce17ebf0048f2aa4e8373a66
Jaya Cahya <jayacahya2008@gmail.com>
2026-02-10 15:21:37 +0700

    Update halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,3 @@
 <h1>ini adalah about</h1>
+
+<h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur earum non molestias consequuntur perferendis veniam aperiam, distinctio eaque fugiat maiores, soluta temporibus dolores.</h3>
