e12766e663adeb8effd3c539961f5e20846206df
Jaya Cahya <jayacahya2008@gmail.com>
2026-02-11 08:33:43 +0700

    menambahkan text di about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <h1>ini adalah about</h1>
 
 <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur earum non molestias consequuntur perferendis veniam aperiam, distinctio eaque fugiat maiores, soluta temporibus dolores.</h3>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta provident saepe, modi rem veritatis porro cum corrupti iure quo a tempore cumque maxime neque quos adipisci, dignissimos quis, laudantium id!
+</p>
