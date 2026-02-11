75c3dfaef325c1ed21fc7147f60816f415b14569
barnyster <mhmdnurakbar1928@gmail.com>
2026-02-11 13:57:09 +0700

    fix: perbaikan tampilan home dan routing

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about'); // Memberikan nama 'about' pada rute
+
+// Rute untuk halaman contact
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact'); // Memberikan nama 'contact' pada rute
